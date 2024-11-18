0d9de6e0b24593ef7f05bdc63255df04db46641d
HikariTempest <arwed@gmx.at>
2024-11-18 09:40:38 +0100

    feat: implemented a component with a form to submit a photo

--- memoryland/src/app/app.component.ts
+++ memoryland/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {PhotoFormComponent} from './components/photo-form/photo-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    PhotoFormComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ memoryland/src/app/components/photo-form/photo-form.component.ts
@@ -0,0 +1,47 @@
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {PhotoService} from '../../services/photo.service';
+
+@Component({
+  selector: 'app-photo-form',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './photo-form.component.html',
+  styleUrl: './photo-form.component.css'
+})
+export class PhotoFormComponent {
+  protected uploadForm: FormGroup;
+  private photoSvc = inject(PhotoService);
+  private fb = inject(FormBuilder);
+
+  constructor() {
+    this.uploadForm = this.fb.group({
+      fileName: ['', Validators.required],
+      photoAlbumId: ['', Validators.required],
+      photo: [null, Validators.required],
+    });
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+
+    if (file) {
+      this.uploadForm.patchValue({ photo: file });
+    }
+  }
+
+  onSubmit() {
+    if (this.uploadForm.valid) {
+      const fileName = this.uploadForm
+        .get('fileName')?.value;
+      const photoAlbumId = this.uploadForm
+        .get('photoAlbumId')?.value;
+      const photo = this.uploadForm
+        .get('photo')?.value;
+
+      this.photoSvc.submitPhoto(fileName, photoAlbumId, photo);
+    }
+  }
+}
