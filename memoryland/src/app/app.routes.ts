bb8944d008906cbc9ae33d6ac0fe9c412667edf6
HikariTempest <arwed@gmx.at>
2024-12-22 16:03:39 +0100

    feat: added msal route guarding

--- memoryland/src/app/app.routes.ts
+++ memoryland/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {FailedComponent} from './components/authentication-and-authorization/failed/failed.component';
+import {ProfileComponent} from './components/authentication-and-authorization/profile/profile.component';
+import {MsalGuard} from '@azure/msal-angular';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login-failed',
+    component: FailedComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [MsalGuard]
+  }
+];
