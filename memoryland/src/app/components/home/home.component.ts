f5e6bc4508807cb19259096c5080cd384ce30663
HikariTempest <arwed@gmx.at>
2024-12-26 17:38:32 +0100

    feat: now the home component reacts on a successful login

--- memoryland/src/app/components/home/home.component.ts
+++ memoryland/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {MsalBroadcastService, MsalService} from '@azure/msal-angular';
+import {filter} from 'rxjs';
+import {AuthenticationResult, EventMessage, EventType, InteractionStatus} from '@azure/msal-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +9,39 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  protected loginDisplay: boolean = false;
 
+  private authService = inject(MsalService);
+  private msalBroadcastService = inject(MsalBroadcastService);
+
+  ngOnInit(): void {
+    this.msalBroadcastService.msalSubject$
+      .pipe(
+        filter((msg: EventMessage) =>
+          msg.eventType === EventType.LOGIN_SUCCESS)
+      )
+      .subscribe((result: EventMessage) => {
+        const payload = result.payload as AuthenticationResult;
+        this.authService
+          .instance
+          .setActiveAccount(payload.account);
+      });
+
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) =>
+          status === InteractionStatus.None)
+      )
+      .subscribe(() => {
+        this.setLoginDisplay();
+      });
+  }
+
+  setLoginDisplay() {
+    this.loginDisplay = this.authService
+      .instance
+      .getAllAccounts()
+      .length > 0;
+  }
 }
