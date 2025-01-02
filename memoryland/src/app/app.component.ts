954f6c403afd6a7d550d0d3674b6955c022862f4
HikariTempest <arwed@gmx.at>
2025-01-02 15:32:41 +0100

    feat: now the environment file get's replaced in prod mode

--- memoryland/src/app/app.component.ts
+++ memoryland/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {HeaderComponent} from "./components/Basic/header/header.component";
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/Basic/header/header.component";
 
 @Component({
   selector: 'app-root',
