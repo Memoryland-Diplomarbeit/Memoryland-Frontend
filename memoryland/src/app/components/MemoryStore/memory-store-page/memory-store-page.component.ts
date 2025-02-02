acf6b16c1f62e8d7c5047e6678ca1c756480ed6a
HikariTempest <arwed@gmx.at>
2025-02-02 15:11:44 +0100

    feat: added a basic first version of the MemoryStore

--- memoryland/src/app/components/MemoryStore/memory-store-page/memory-store-page.component.ts
+++ memoryland/src/app/components/MemoryStore/memory-store-page/memory-store-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {FolderListComponent} from '../folder-list/folder-list.component';
+import {ImageListComponent} from '../image-list/image-list.component';
 
 @Component({
   selector: 'app-memory-store-page',
-  imports: [],
+  imports: [
+    FolderListComponent,
+    ImageListComponent
+  ],
   templateUrl: './memory-store-page.component.html',
   styleUrl: './memory-store-page.component.css'
 })
 export class MemoryStorePageComponent {
 
+  createAlbum() {
+
+  }
 }
