921650cefe26d02ab921c107b036a927f052ed14
schnalzenberger0502 <i.schnalzenberger@students.htl-leonding.ac.at>
2025-02-13 22:37:17 +0100

    fix: now the unity-connection worked again (I needed a dom-sanitizer for the generated url)

--- memoryland/src/app/components/Memoryland/explore-worlds-page/explore-worlds-page.component.ts
+++ memoryland/src/app/components/Memoryland/explore-worlds-page/explore-worlds-page.component.ts
@@ -1,8 +1,9 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {store} from '../../../model';
-import {distinctUntilChanged, map} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs';
 import {WebapiService} from '../../../services/webapi.service';
 import {AsyncPipe} from '@angular/common';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-explore-worlds-page',
@@ -14,21 +15,34 @@ import {AsyncPipe} from '@angular/common';
 })
 export class ExploreWorldsPageComponent implements OnInit {
   protected webApi = inject(WebapiService);
+  protected sanitizer = inject(DomSanitizer);
+  safeUrl: SafeResourceUrl | null = null;
 
   protected token = store
     .pipe(
       map(model => model.token),
       distinctUntilChanged(),
+      debounceTime(1500)
     );
 
   ngOnInit(): void {
     store.pipe(
         map(model => model.selectedMemoryland),
         distinctUntilChanged(),
+        debounceTime(1500)
       ).subscribe(memoryland => {
         if (memoryland !== undefined) {
           this.webApi.getPrivateToken(memoryland.id);
         }
+    });
+
+    this.token.subscribe(token => {
+      if (token !== null && token !== undefined && token !== "") {
+        this.safeUrl = this.sanitizer
+          .bypassSecurityTrustResourceUrl(`/unity/index.html?token=${token}`);
+      } else {
+        this.safeUrl = null;
+      }
     });
   }
 }
