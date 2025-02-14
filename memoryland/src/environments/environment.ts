e75fd7db2df75cb929e1c48e77e4995fafd070b5
Hikari Tempest (Arwed S.) <91137888+HikariTempest@users.noreply.github.com>
2025-02-14 12:17:59 +0100

    Create environment.ts

---
+++ memoryland/src/environments/environment.ts
@@ -0,0 +1,35 @@
+const b2cPolicies = {
+  names: {
+    signUpSignIn: window["env"]?.B2C_SIGNUP_SIGNIN_FLOW!,
+    editProfile: window["env"]?.B2C_EDIT_PROFILE_FLOW!
+  },
+  authorities: {
+    signUpSignIn: {
+      authority: window["env"]?.B2C_SIGNUP_SIGNIN_AUTHORITY!
+    },
+    editProfile: {
+      authority: window["env"]?.B2C_EDIT_PROFILE_AUTHORITY!
+    }
+  },
+  authorityDomain: window["env"]?.B2C_AUTHORITY_DOMAIN!
+};
+
+export const environment = {
+  production: false,
+  b2cPolicies: b2cPolicies,
+  msalConfig: {
+    auth: {
+      clientId: window["env"]?.CLIENT_ID!,
+      authority: b2cPolicies.authorities.signUpSignIn.authority,
+      knownAuthorities: [b2cPolicies.authorityDomain],
+      redirectUri: '/',
+    }
+  },
+  apiConfig: {
+    scopes: [
+      window["env"]?.SCOPE_READ!,
+      window["env"]?.SCOPE_WRITE!
+    ],
+    uri: window["env"]?.BACKEND_URI!
+  }
+};
